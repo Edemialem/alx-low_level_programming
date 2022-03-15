1d34da40d6a691189d82e9c4b2219c472f3469e8
Edemialem <21edemialemtamiru@gmail.com>
2022-03-14 19:25:50 -0700

    101 advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,7 @@
 
 /** main - Prints numbers between 012 to 789.
  * Return: Always 0 (Success)
- II*/
+ */
 
 int main(void)
 
@@ -23,43 +23,32 @@ while (i < 58)
 
 {
 
-																			
 g = 48;
 
-																			
 while (g < 58)
 
-																			
 {
 
-																			
 if (e != i && e != g && i != g && e < i && i < g)
 
 {
-
 putchar(e);
 putchar(i);
 putchar(g);
 if (i == 56 && e == 55 && g == 57)
 
 {
-
-break;	}
-
-																																																		putchar(',');
+break;
+}
+putchar(',');
 putchar(' ');
 }
 g++;
-
 }
-
 i++;
-
 }
-
 e++;
 }
-
 putchar('\n');
 
 return (0);
