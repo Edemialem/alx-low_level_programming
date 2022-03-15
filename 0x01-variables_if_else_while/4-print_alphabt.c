e8e5bc2635d621d94704ae6b72cbcf5a24140d56
Edemialem <21edemialemtamiru@gmail.com>
2022-03-14 17:05:08 -0700

    4-print alphaabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *
  *  main - Prints the alphabet without q and e.
-  * Return: Always 0 (Success)
- *
-      */
+ *  Return: Always 0 (Success)
+ */
 
 int main(void)
 
