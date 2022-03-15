4a7020fcd109fe4d8bf0c09cadce633824cace74
Edemialem <21edemialemtamiru@gmail.com>
2022-03-14 19:55:00 -0700

    101 advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,36 +12,25 @@ int main(void)
 {
 int i, e, g, h, op1, op2;
 i = e = g = h = 48;
-
 while (h < 58)
 
 {
-
 g = 48;
-
 while (g < 58)
-
 {
 
 e = 48;
-
-while (e < 58)
-
-										{
-
+while (e < 58)									{
 										i = 48;
-
 while (i < 58)
 
 {
 
 op1 = (h * 10) + g;
 op2 = (e * 10) + i;
-
 if (op1 < op2)
 
 {
-
 putchar(h);
 putchar(g);
 putchar(' ');
@@ -59,9 +48,7 @@ e++;
 g++;
 }
 h++;
-
 }
-
 putchar('\n');
 
 return (0);
