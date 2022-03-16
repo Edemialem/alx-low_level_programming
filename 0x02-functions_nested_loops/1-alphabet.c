cf730a06b37bb2b3da9452f63364f527c45fb04a
Edemialem <21edemialemtamiru@gmail.com>
2022-03-16 02:32:41 -0700

    print putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- *   print_alphabet - print alphabet lowercase
- *
- *   
- *
- *     Return: Always 0.
+ * print_alphabet - print alphabet lowercase
+ * Return: Always 0.
  *
  */
 void print_alphabet(void)
