6d768a9aebb53e02dddef2432718262dff23f1ab
Edemialem <21edemialemtamiru@gmail.com>
2022-03-16 03:14:48 -0700

    print putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -18,15 +19,13 @@ void times_table(void)
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
-	   		{
+				{
 					_putchar ('0');
 				}
-
 				if (b != 0)
            			{
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
-
 				}
 				if (b < 9)
 				{
