1ecb71e13a03eaef1954a057433d1513458f61f6
Edemialem <21edemialemtamiru@gmail.com>
2022-03-17 10:50:52 -0700

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,15 +6,20 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 /*
 *while (i < 10)
 *{
 *Infinite loop - No increase of variabl
 * putchar(i);
 *}
-*/	
+*/
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
