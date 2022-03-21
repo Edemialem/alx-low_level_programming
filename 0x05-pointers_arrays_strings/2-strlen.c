1fc5ffe839f75a7e76a765afd82cd8ed7c3ec623
Edemialem <21edemialemtamiru@gmail.com>
2022-03-21 07:07:14 -0700

    commit on string and pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *   _strlen - return the length of the string
  *
@@ -18,7 +16,9 @@ int _strlen(char *s)
 	int c;
 
 	for (c = 0; s[c] != '\0'; c++)
-				
-				;
+			
+	;
+
 	return (c);
+
 }
